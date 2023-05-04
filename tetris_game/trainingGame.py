4d6a1a24cf1671c2aba201ea0ddfd515ebff19d5
Patrick Chen <patrickchen@Patricks-MacBook-Pro.local>
2023-05-04 19:44:05 -0400

    Created game to train AI in background

--- tetris_game/trainingGame.py
+++ tetris_game/trainingGame.py
@@ -74,7 +74,6 @@ class Tetris:
 
     def new_figure(self):
         self.figure = Figure(0, 0)
-        self.best_moves(-1, -1, 1, -1)
 
     def intersects(self):
         intersection = False
@@ -108,18 +107,11 @@ class Tetris:
         self.figure.y -= 1
         self.freeze()
 
-    # simulates moving the block down to the furthest it goes
     def test_space(self):
         while not self.intersects():
             self.figure.y += 1
         self.figure.y -= 1
         return self.test_freeze()
-
-    def go_down(self):
-        self.figure.y += 1
-        if self.intersects():
-            self.figure.y -= 1
-            self.freeze()
 
     def freeze(self):
         for i in range(4):
@@ -132,7 +124,6 @@ class Tetris:
         if self.intersects():
             self.state = "gameover"
 
-    # fills out the board and checks if the block is out of the board limits
     def test_freeze(self):
         for i in range(4):
             for j in range(4):
@@ -222,7 +213,7 @@ class Tetris:
         background.clone(self)
         curr = background.figure
         max_score = -1000000
-        best_move = [0, 0, 0]
+        best_move = [0, 0]
 
         for i in range(forms):
             curr.rotate(i)
@@ -236,83 +227,23 @@ class Tetris:
                     + param3 * background.complete_lines() + param4 * background.holes()
                     if(curr_score >= max_score):
                         max_score = curr_score
-                        best_move = [j, i, curr.y]
+                        best_move = [j, i]
         
         self.figure.rotate(best_move[1])
         self.figure.x = best_move[0]
-        self.figure.y = best_move[2]
+        self.go_space()
 
-
-
-        
-# Initialize the game engine
-pygame.init()
-
-# Define some colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GRAY = (128, 128, 128)
-
-size = (400, 500)
-screen = pygame.display.set_mode(size)
-
-pygame.display.set_caption("Tetris")
-
-# Loop until the user clicks the close button.
-done = False
-clock = pygame.time.Clock()
-fps = 25
-game = Tetris(20, 10)
-counter = 0
-
-while not done:
-    if game.figure is None:
+def trainingGame(param1, param2, param3, param4, rounds, moves):
+    list = []
+    for i in range(rounds):
+        game = Tetris(20, 10)
         game.new_figure()
-    counter += 1
-    if counter > 100000:
-        counter = 0
+        for j in range(moves):
+            if game.state == "gameover":
+                break
+            game.best_moves(param1, param2, param3, param4)
+        list.append(game.score)
+    return list
 
-    if counter % (fps // game.level // 2) == 0:
-        if game.state == "start":
-            game.go_down()
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-
-    screen.fill(WHITE)
-
-    for i in range(game.height):
-        for j in range(game.width):
-            pygame.draw.rect(screen, GRAY, [
-                             game.x + game.zoom * j, game.y + game.zoom * i, game.zoom, game.zoom], 1)
-            if game.field[i][j] > 0:
-                pygame.draw.rect(screen, colors[int(game.field[i][j])],
-                                 [game.x + game.zoom * j + 1, game.y + game.zoom * i + 1, game.zoom - 2, game.zoom - 1])
-
-    if game.figure is not None:
-        for i in range(4):
-            for j in range(4):
-                p = i * 4 + j
-                if p in game.figure.image():
-                    pygame.draw.rect(screen, colors[game.figure.color],
-                                     [game.x + game.zoom * (j + game.figure.x) + 1,
-                                      game.y + game.zoom *
-                                      (i + game.figure.y) + 1,
-                                      game.zoom - 2, game.zoom - 2])
-
-    font = pygame.font.SysFont('Calibri', 25, True, False)
-    font1 = pygame.font.SysFont('Calibri', 65, True, False)
-    text = font.render("Score: " + str(game.score), True, BLACK)
-    text_game_over = font1.render("Game Over", True, (255, 125, 0))
-    text_game_over1 = font1.render("Press ESC", True, (255, 215, 0))
-
-    screen.blit(text, [0, 0])
-    if game.state == "gameover":
-        screen.blit(text_game_over, [20, 200])
-        screen.blit(text_game_over1, [25, 265])
-
-    pygame.display.flip()
-    clock.tick(fps)
-
-pygame.quit()
+print(trainingGame(-1, -1, 1, -1, 10, 100))
+        

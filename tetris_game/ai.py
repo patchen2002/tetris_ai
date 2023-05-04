27442ccfd4b398c2a001273ac4ffa05c94d5246e
Corfish123 <80220215+Corfish123@users.noreply.github.com>
2023-05-04 19:43:59 -0400

    finished training algorithm

--- tetris_game/ai.py
+++ tetris_game/ai.py
@@ -5,4 +5,3 @@ class AI:
         self.holesWeight = holesWeight
         self.bumpinessWeight = bumpinessWeight
         self.fitness = 0
-    
--- tetris_game/trainingAlgorithm.py
+++ tetris_game/trainingAlgorithm.py
@@ -2,6 +2,7 @@ import math
 import ai
 import random
 import numpy as np
+from pprint import pprint
 class trainingAlgorithm:
     def normalize(ai):
         norm = math.sqrt(ai.heightWeight**2 + ai.linesWeight**2 + ai.holesWeight**2 + ai.bumpinessWeight**2)
@@ -59,4 +60,27 @@ class trainingAlgorithm:
         trainingAlgorithm.sort(allAi)
         return allAi
     
+    def training(population, rounds, maxMoves):
+        allAi = np.array(trainingAlgorithm.generateNewAi() for i in range(len(population)))
+        print("finish initial population")
+        # computeFitness(allAi, rounds, maxMoves) #Patrick implements
+        trainingAlgorithm.sort(allAi)
+
+        newAi = np.array()
+        for i in range(30):
+            parents = trainingAlgorithm.tournamentSelect(allAi)
+            print('fitnesses = ' + parents[0].fitness + ',' + parents[1].fitness)
+            child = trainingAlgorithm.crossOver(parents[0], parents[1])
+            if random.random() < 0.05:
+                trainingAlgorithm.mutate(child)
+            newAi.append(child)
+
+        # computeFitness(allAi, rounds, maxMoves) #Patrick implements
+        trainingAlgorithm.deleteAndReplace(allAi,newAi)
+        totalFitness = 0
+        for ai in allAi:
+            totalFitness += ai.fitness
+        print("Average Fitness: ", totalFitness/len(allAi))
+        print("Best AI: ")
+        pprint(vars(allAi[0]))
         
